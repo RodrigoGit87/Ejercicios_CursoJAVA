e1f5698760ed5e1b8fd9f026cb559ae818a107f2
RodrigoGit87 <rodrigollamas87@gmail.com>
2025-12-08 11:48:00 +0100

    .

---
+++ Ejercicios_Polimorfismo/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    void main(String[] args) {
+    IO.println("Hello world");
+
+        //Ejercicio 1
+        // Crear una lista cuyo TIPO sea la Superclase (Animal).
+//        List<Animal> zoo = new ArrayList<>();
+        // Añadir instancias de las subclases a la lista de Animal.
+//        zoo.add(new Animal.Dog("cancerbero","perro"));
+//        zoo.add(new Animal.Cat("Garfield","gato"));
+//        zoo.add(new Animal.Cow("Lola","Vaca"));
+//        //llamada a metodo
+//        for (Animal animal : zoo){
+//            animal.makeSound();
+//        }
+
+    }
+}
